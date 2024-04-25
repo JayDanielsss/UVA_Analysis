d2b4fbf5f25d06e566359b8f38c66a747c91295c
JayDanielsss <jordandrobertss@gmail.com>
2024-04-25 11:53:42 -0400

    Added 2nd Window. Fixed bug with hitdisplay. Added readout.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow, QLabel,QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTabWidget, QPushButton, QGridLayout
+from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem,QLabel,QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTabWidget, QPushButton, QGridLayout
 import pyqtgraph as pg
 import numpy as np
 from DataReader import DataReader
@@ -13,6 +13,145 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 import matplotlib.pyplot as plt
+from SpillCharts import SpillCharts
+from StripCharts import StripCharts
+from VertexHists import VertHists
+
+
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+# class OccPlotter(QWidget):
+#     def __init__(self,DC,Hodo,Prop):
+#         super().__init__()
+
+#         layout = QHBoxLayout()
+#         self.setLayout(layout)
+
+#         # Create a PlotWidget to display the hit matrix
+#         self.plot_widget = pg.PlotWidget()
+#         layout.addWidget(self.plot_widget,stretch = 2)
+       
+#         self.plot_Occ(DC,Title="Drift Chamber Occ")
+#         self.plot_widget = pg.PlotWidget()
+#         layout.addWidget(self.plot_widget,stretch = 2)
+
+#         self.plot_Occ(Hodo,Title="Hodo Occ")
+
+#         self.plot_widget = pg.PlotWidget()
+#         layout.addWidget(self.plot_widget,stretch = 2)
+#         self.plot_Occ(Prop,Title="Prop Occ")
+
+#         layout.addWidget(VertHists(),stretch = 8)
+
+
+#     def plot_Occ(self,Occ,Title):
+#         y,x = np.histogram(Occ.flatten(), bins=20)
+#         hist = pg.PlotCurveItem(x, y, stepMode=True, fillLevel=0, brush=(0, 0, 255, 80))
+#         self.plot_widget.setTitle(Title)
+#         self.plot_widget.addItem(hist)
+
+
+        
+        
+
+class HitMatrixPlotter(QWidget):
+    def __init__(self, hitmatrix,Station,Plane, x_range=None, y_range=None,Title=None):
+        super().__init__()
+
+        # Create a layout to hold the plot widget
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        # Create a PlotWidget to display the hit matrix
+        self.plot_widget = pg.PlotWidget()
+        layout.addWidget(self.plot_widget)
+
+        # Plot the hit matrix
+        self.plot_hitmatrix(hitmatrix)
+
+        # Set custom axis ranges if provided
+        if x_range is not None:
+            self.plot_widget.setXRange(*x_range)
+        if y_range is not None:
+            self.plot_widget.setYRange(*y_range)
+        if Title is not None:
+            self.plot_widget.setTitle(Title)
+
+        # Create QLabel for readout
+        self.readout_label = QLabel(alignment=Qt.AlignLeft)
+        layout.addWidget(self.readout_label)
+
+        # Update readout
+        # self.update_readout(70, 0.7)
+        self.getStationOcc(Station,Plane)
+
+    def plot_hitmatrix(self, hitmatrix):
+        # Extract data from the hit matrix
+        elemid = hitmatrix[:, :, 1].flatten()
+        detid = hitmatrix[:, :, 0].flatten()
+
+        # Create a scatter plot
+        scatter_plot = pg.ScatterPlotItem()
+        scatter_plot.setData(x=detid, y=elemid, pen=None, symbol='s')
+
+        # Set labels for each axis
+        self.plot_widget.setLabel('bottom', text='Det ID')
+        self.plot_widget.setLabel('left', text='Elem ID')
+
+        # Add scatter plot to the PlotWidget
+        self.plot_widget.addItem(scatter_plot)
+
+    def getStationOcc(self,detector,Plane):
+
+        geometery = np.loadtxt("geometery.csv", delimiter=",", dtype=str)
+
+        PerEvent = np.zeros_like(detector)
+        
+
+        for i in range(len(detector)):
+            for j in range(len(detector[0])):
+                PerEvent[i,j] = detector[i,j]/int(geometery[Plane[j]][1])
+                
+
+
+        AvgOccPerSpill = np.average(PerEvent)
+        TotalOccPerSpill = np.sum(detector)
+
+
+
+        text = f"Occupancy\nAvg Hits per Event: {AvgOccPerSpill*100:.0f}%\nTotal Hits per Spill: {TotalOccPerSpill}\n"
+        self.readout_label.setText(text)
+
+class MyTable(QTableWidget):
+    def __init__(self, rows, columns):
+        super().__init__(rows, columns)
+        self.setGeometry(0, 0, 400, 300)
+
+        # Insert data into the table
+        self.setData()
+
+    def setData(self):
+        # Example data
+        data = [
+            ("Run ID", 0),
+            ("Spill ID",  0),
+            ("Total Hits",  0),
+            
+        ]
+
+
+        # Populate the table with data
+        for row, rowData in enumerate(data):
+            for col, value in enumerate(rowData):
+                item = QTableWidgetItem(str(value))
+                self.setItem(row, col, item)
+
+
+
+
+    
 
 class StaticHistogram(QWidget):
     def __init__(self,data):
@@ -57,87 +196,6 @@ class StaticHistogram(QWidget):
             self.layout().addLayout(plot_layout)
 
 
-class HitMatrixPlotter:
-    def __init__(self, hitmatrix):
-        super().__init__()
-        self.figure = Figure(figsize=(2, 3))
-        self.canvas = FigureCanvas(self.figure)
-        # Dictionary to store subplot references
-        subplots = {}
-
-        # Add subplots in a 1x12 grid
-        for i in range(1, 11):
-            self.ax = self.figure.add_subplot(1, 10, i)
-            subplots[f'ax{i}'] = self.ax  # Store subplot reference in the dictionary
-
-        # for i in range(1, 11):
-        #     self.ax = self.figure.add_subplot(2, 10, i)
-        #     subplots[f'ax{i+10}'] = self.ax  # Store subplot reference in the dictionary
-
-
-        # self.ax1.clear()
-        # self.ax2.clear()
-        elemid = hitmatrix[:,:,1]
-        detid = hitmatrix[:,:,0]
-        # #self.ax1.imshow(event[:32, :], cmap='binary', aspect='auto')
-        subplots['ax1'].scatter(detid,elemid, marker=',')
-        subplots['ax1'].set_xlim(29.5,33.5)
-        subplots['ax1'].set_ylim(0,23.5)
-        subplots['ax1'].set_title("Hodo: 1")
-
-        subplots['ax2'].scatter(detid,elemid,marker=',')
-        subplots['ax2'].set_xlim(-.5,6.5)
-        subplots['ax2'].set_ylim(0,201.5)
-        subplots['ax2'].set_title("DC: ST1")
-
-        subplots['ax3'].scatter(detid,elemid,marker=',')
-        subplots['ax3'].set_xlim(11.5,17.5)
-        subplots['ax3'].set_ylim(0,128.5)
-        subplots['ax3'].set_title("DC: ST2")
-
-        subplots['ax4'].scatter(detid,elemid,marker=',')
-        subplots['ax4'].set_xlim(33.5,37.5)
-        subplots['ax4'].set_ylim(0,19.5)
-        subplots['ax4'].set_title("Hodo: 2")
-
-        subplots['ax5'].scatter(detid,elemid,marker=',')
-        subplots['ax5'].set_xlim(17.5,23.5)
-        subplots['ax5'].set_ylim(0,134.5)
-        subplots['ax5'].set_title("DC: ST3M")
-
-        subplots['ax6'].scatter(detid,elemid,marker=',')
-        subplots['ax6'].set_xlim(23.5,29.5)
-        subplots['ax6'].set_ylim(0,134.5)
-        subplots['ax6'].set_title("DC: ST3P")
-
-        subplots['ax7'].scatter(detid,elemid,marker=',')
-        subplots['ax7'].set_xlim(37.5,39.5)
-        subplots['ax7'].set_ylim(0,16.5)
-        subplots['ax7'].set_title("Hodo: 3")
-
-        subplots['ax8'].scatter(elemid,detid,marker=',')
-        subplots['ax8'].set_xlim(45.5,79.5)
-        subplots['ax8'].set_ylim(-1,8.5)
-        subplots['ax8'].set_title("Prop Tube 1")
-
-        subplots['ax9'].scatter(detid,elemid,marker=',')
-        subplots['ax9'].set_xlim(39.5,45.5)
-        subplots['ax9'].set_ylim(0,16.5)
-        subplots['ax9'].set_title("Hodo: 4")
-
-        subplots['ax10'].scatter(elemid,detid,marker=',')
-        subplots['ax10'].set_xlim(79.5,117.5)
-        subplots['ax10'].set_ylim(-1,8.5)
-        subplots['ax10'].set_title("Prop Tube 2")
-
-
-
-
-
-        self.figure.subplots_adjust(wspace=.4) 
-        self.figure.subplots_adjust(hspace=1) 
-        
-        self.canvas.draw()
 
 class HistogramComparisonPlot(QWidget):
     def __init__(self, vtx_data, previous_data=None):
@@ -167,7 +225,7 @@ class Tab1(QWidget):
     def __init__(self):
         super().__init__()
         #layout = QGridLayout()
-        layout = QVBoxLayout()
+        layout = QVBoxLayout()  # Create one QVBoxLayout
         self.setLayout(layout)
 
 
@@ -193,7 +251,7 @@ class Tab1(QWidget):
         num_events = 20 #len(hits)
         hitmatrix = np.zeros((num_events,1000,2))
 
-        DriftChamber = np.zeros((num_events,4))
+        DriftChamber = np.zeros((num_events,24))
         Hodoscope = np.zeros((num_events,15))
         propTube = np.zeros((num_events,7))
 
@@ -209,35 +267,80 @@ class Tab1(QWidget):
             propTube[i] = Prop
 
 
-
-        # print((np.sum(DriftChamber[:,0]),np.sum(DriftChamber[:,1]),np.sum(DriftChamber[:,2]),np.sum(DriftChamber[:,3])))
-        # print(np.sum(Hodoscope))
-        # print(np.sum(propTube))
-        # plt.scatter(hitmatrix[0,:,0],hitmatrix[0,:,1])
-        # plt.xlim(-.5,6.5)
-        # plt.show()
-        # print(hitmatrix[hitmatrix[:,:,0] != 0][:,0])
-        # plt.hist(hitmatrix[hitmatrix[:,:,1] != 0][:,0], bins=60, color='skyblue', edgecolor='black')
-        # plt.xlim(-.5,6.5)
-        # plt.show()
-
-
-        # #n
-        # self.event = 0
-        self.hit_matrix_plotter = HitMatrixPlotter(hitmatrix)
-        layout.addWidget(self.hit_matrix_plotter.canvas)
-  
-  
+        
         
 
+        
 
- 
+        
+    
+
+
+        #Hitmatrix Display
+        hit_layout = QHBoxLayout()
+
+        x_range = (29, 33)
+        y_range = (0, 23)
+        #Hodo1
+        self.hit_matrix_plotter = HitMatrixPlotter(hitmatrix,Station=Hodoscope[:,:4],Plane=np.arange(6,10),x_range=x_range, y_range=y_range,Title="Hodo: 1")
+        hit_layout.addWidget(self.hit_matrix_plotter,stretch=3)
+
+        
+
+        x_range = (0, 6)
+        y_range = (0, 201)
+        #DC1
+        self.hit_matrix_plotter2 = HitMatrixPlotter(hitmatrix,Station=DriftChamber[:,:6],Plane=np.arange(0,6),x_range=x_range, y_range=y_range,Title="DC St: 1")
+        hit_layout.addWidget(self.hit_matrix_plotter2,stretch=3)
+
+        x_range = (12, 17)
+        y_range = (0, 128)
+        #DC2
+        self.hit_matrix_plotter3 = HitMatrixPlotter(hitmatrix,Station=DriftChamber[:,6:12],Plane=np.arange(20,26),x_range=x_range, y_range=y_range,Title="DC St: 2")
+        hit_layout.addWidget(self.hit_matrix_plotter3,stretch=3)
+
+        x_range = (33, 37)
+        y_range = (0, 19)
+        #Hodo2
+        self.hit_matrix_plotter4 = HitMatrixPlotter(hitmatrix,Station=Hodoscope[:,4:8],Plane=np.arange(26,30),x_range=x_range, y_range=y_range,Title="Hodo: 2")
+        hit_layout.addWidget(self.hit_matrix_plotter4,stretch=3)
+
+        x_range = (17, 23)
+        y_range = (0, 134)
+        #DC3M
+        self.hit_matrix_plotter5 = HitMatrixPlotter(hitmatrix,Station=DriftChamber[:,12:18],Plane=np.arange(34,40),x_range=x_range, y_range=y_range,Title="DC St:3m")
+        hit_layout.addWidget(self.hit_matrix_plotter5,stretch=3)
+
+        x_range = (23, 29)
+        y_range = (0, 134)
+        #DC3P
+        self.hit_matrix_plotter6 = HitMatrixPlotter(hitmatrix,Station=DriftChamber[:,18:24],Plane=np.arange(40,46),x_range=x_range, y_range=y_range,Title="DC St:3p")
+        hit_layout.addWidget(self.hit_matrix_plotter6,stretch=3)
+
+        x_range = (37, 45)
+        y_range = (0, 16)
+        #Hodo 3 & 4
+        self.hit_matrix_plotter7 = HitMatrixPlotter(hitmatrix,Station=Hodoscope[:,8:20],Plane=[46,47,66,67,86,87,88,89],x_range=x_range, y_range=y_range,Title="Hodo: 3 & 4")
+        hit_layout.addWidget(self.hit_matrix_plotter7,stretch=3)
+
+        x_range = (45, 54)
+        y_range = (0, 72)
+
+        #Prop
+        self.hit_matrix_plotter8 = HitMatrixPlotter(hitmatrix,Station=propTube,Plane=[8,8,8,8,8,8,8,8,8],x_range=x_range, y_range=y_range,Title="Prop Tubes")
+        hit_layout.addWidget(self.hit_matrix_plotter8,stretch=3) 
+        
+        Readout = MyTable(3,2)
+        hit_layout.addWidget(Readout, stretch=3)
+
+        layout.addLayout(hit_layout,stretch=2)
 
         # Add update button
         self.update_button = QPushButton("Update")
         self.update_button.clicked.connect(self.update_plots)
         layout.addWidget(self.update_button)
 
+        
        
 
 
@@ -257,9 +360,36 @@ class Tab2(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        layout.addWidget(pg.ImageView())
         self.setLayout(layout)
 
+        vertexStrip_layout = QVBoxLayout()
+        vertexStrip_layout.addWidget(SpillCharts())
+        vertexStrip_layout.addWidget(StripCharts())
+
+
+        layout.addLayout(vertexStrip_layout,stretch = 4) 
+
+
+
+class StripChartWindow(QMainWindow):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        super().__init__()
+        self.title = 'SpinQuest Display'
+        self.left = 0
+        self.top = 0
+        self.width = 800
+        self.height = 400
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        tabs = QTabWidget()
+        tab2 = Tab2()
+        tabs.addTab(tab2, "StripCharts")
+        self.setCentralWidget(tabs)
 
 class App(QMainWindow):
     def __init__(self):
@@ -275,9 +405,11 @@ class App(QMainWindow):
         tabs = QTabWidget()
         tab1 = Tab1()
         tab2 = Tab2()
+        #tab3 = SpillCharts()
 
         tabs.addTab(tab1, "Main Display")
-        tabs.addTab(tab2, "Hit Information")
+        
+       # tabs.addTab(tab3, "Spill")
         self.setCentralWidget(tabs)
 
 
@@ -285,4 +417,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = App()
     window.show()
+
+    window2 = StripChartWindow()
+    window2.show()
     sys.exit(app.exec_())
