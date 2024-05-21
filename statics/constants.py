55b8637326a0c1d46573738b6b37df4ef60451dd
malschloss <166645117+malschloss@users.noreply.github.com>
2024-05-21 14:06:26 -0400

    DIRECTORY HEALTH!!!!!!
    it works now i think

---
+++ statics/constants.py
@@ -0,0 +1,33 @@
+# Application Name:
+
+# Application | SpinQuest Display
+_APPLICATION_NAME = "SpinQuest Display"
+
+
+# Window Data:
+
+# Window Name | SpinQuest Display
+_WINDOW_MAIN_APP = "SpinQuest GUI"
+
+# Window Dimensions | SpinQuest Display
+_WINDOW_MAIN_APP_WIDTH = 800
+_WINDOW_MAIN_APP_HEIGHT = 400
+
+
+# Tabs | Main Display Tab
+_TAB_MAIN_NAME = "Main Display"
+
+# Tabs | StripCharts Tab
+_TAB_STRIPCHARTS = "StripCharts"
+
+#Directory Names | SpinQuest GUI
+_SPINQUEST_GUI = "spinquest_gui"
+
+# Directory Names | Data 
+_DIRECTORY_DATA = "data"
+
+# Directory Names | Data | Raw
+_DIRECTORY_DATA_RAW = "raw"
+
+# Directory Names | Data | Reconstructed
+_DIRECTORY_DATA_RECONSTRUCTED = "reconstructed"
