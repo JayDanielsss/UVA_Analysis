cdbf8a66fd2cb5d04537e1d030913f36ae20c351
malschloss <166645117+malschloss@users.noreply.github.com>
2024-05-22 13:37:01 -0400

    Organizer conversions
    Added refresh functions to main and tab 2, created OrganizerMassHist.py and OrganizerStripCharts.py. OrganizerMassHist.py seems to be working as intended, OrganizerStripCharts.py needs more work.

--- spinquest_gui/modules/directories/directory_health.py
+++ spinquest_gui/modules/directories/directory_health.py
@@ -1,6 +1,5 @@
 # Native Package | os
 import os
-import glob
 
 from spinquest_gui.statics.constants import _DIRECTORY_DATA, _DIRECTORY_DATA_RAW, _DIRECTORY_DATA_RECONSTRUCTED, _SPINQUEST_GUI
 
@@ -25,6 +24,27 @@ def check_reconstructed_directory():
 
     except Exception as ERROR:
         print(f"> [ERROR]: {ERROR}")
+
+def check_raw_directory():
+    try:
+
+        path_to_spinquest_gui = f"{os.getcwd()}/{_SPINQUEST_GUI}"
+
+        if not (os.path.exists(path_to_spinquest_gui)):
+            os.mkdir(path_to_spinquest_gui)
+        
+        if not (os.path.exists(f"{path_to_spinquest_gui}/{_DIRECTORY_DATA}")):
+            os.mkdir(f"{path_to_spinquest_gui}/{_DIRECTORY_DATA}")
+
+        if not (os.path.exists(f"{path_to_spinquest_gui}/{_DIRECTORY_DATA}/{_DIRECTORY_DATA_RAW}")):
+            os.mkdir(f"{path_to_spinquest_gui}/{_DIRECTORY_DATA}/{_DIRECTORY_DATA_RAW}")
+        else:
+
+            print(f"> Path {path_to_spinquest_gui}/{_DIRECTORY_DATA}/{_DIRECTORY_DATA_RAW} exists!")
+
+    except Exception as ERROR:
+        print(f"> [ERROR]: {ERROR}")
+
 
 def get_raw_directory():
     """
@@ -56,28 +76,18 @@ def get_reconstructed_directory():
 
     return current_dir
 
-def get_raw_contents():
+def get_reconstructed_contents():
    
-    # Construct the path to the 'raw' directory
-    raw_dir = get_raw_directory()
+    # Construct the path to the 'reconstructed' directory
+    reconstructed_dir = get_reconstructed_directory()
 
     try:
-         #finds the raw file
-        # List all files in the directory
-        contents = glob.glob(os.path.join(raw_dir, '*'))
 
-        # Sort files by modification time, most recent first
-        contents.sort(key=os.path.getmtime, reverse=True)
-
-
-        # contents = os.listdir(raw_dir)
-        # contents.sort(key=lambda x: os.path.getmtime(x), reverse=True)
-
+        contents = os.listdir(reconstructed_dir)
 
     except FileNotFoundError:
 
         contents = []
-        print(f"The directory {raw_dir} does not exist.")
+        print(f"The directory {reconstructed_dir} does not exist.")
     
     return contents
-
