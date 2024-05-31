2e0e13925ceeedb100be70db8ee2d86fc81020ca
JayDanielsss <jordandrobertss@gmail.com>
2024-05-31 11:59:28 -0400

    Made the display module take in and update the plot_widget. Now display module and tab are seperated.

--- app/modules/HitDisplay.py
+++ app/modules/HitDisplay.py
@@ -4,14 +4,56 @@ import numpy as np
 # External Packages | pyqtgraph
 import pyqtgraph as pg
 
-class HitDisplay:
+from PyQt5.QtCore import Qt, QTimer, QObject
 
-    def __init__(self):        
+class HitDisplay(QObject):  # Inherit from QObject
+
+    def __init__(self, elementid, detectorid, selectedEvents, sid, hits, eventID, track, plot_widget):
+        super().__init__()  # Call the constructor of QObject
+        self.elementid = elementid
+        self.detectorid = detectorid
+        self.selectedEvents = selectedEvents
+        self.sid = sid
+        self.hits = hits
+        self.eventID = eventID
+        self.track = track  
+        self.plot_widget = plot_widget  
+
+        self.ith_event = 0 
+        #timer to cycle through events
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.display)  
+        self.timer.start(1000)  # Call hit_display every 1000 milliseconds (1 second)
+
+        if self.ith_event >= len(self.selectedEvents):
+            self.timer.stop()
+            return
+
+    def display(self):
+        #display pyqtgraph setup 
+        if self.ith_event >= len(self.selectedEvents):
+            self.timer.stop()
+            return
+        scatter_raw = self.Raw_Hit()  
+        scatter_cluster = self.Cluster_Hit()  
+        scatter_mup, scatter_mum = self.Track_Hits()  
+
+        # Clear the plot widget before drawing new data
+        self.plot_widget.clear()
+        # Add the scatter plot items to the plot widget
+        self.plot_widget.addItem(scatter_raw)
+        self.plot_widget.addItem(scatter_cluster)                 
+        self.plot_widget.addItem(scatter_mup)
+        self.plot_widget.addItem(scatter_mum)
+
         
-        return None
+        self.plot_widget.setYRange(0, 201)
 
+        # Advance the event index
+        self.ith_event += 1
 
     def getOcc(self, hits, selectedEvents, eventID, ith_event):
+        '''Gets occupancy for each detector and percentages'''
 
         eventIndex = np.where(selectedEvents[ith_event] == eventID)[0]
 
@@ -29,7 +71,6 @@ class HitDisplay:
             index = hitmatrix[hitmatrix[:,0] == i]
             DC[i] = len(index)
 
-
         for i in range(12,17):
             index = hitmatrix[hitmatrix[:,0] == i]
             DC[i-6] = len(index)
@@ -40,8 +81,6 @@ class HitDisplay:
         for i in range(24,29):
             index = hitmatrix[hitmatrix[:,0] == i]
             DC[i-6] = len(index)
-        
-        
 
         for i in range(30,46):
             Hodo[30-i] = len(hitmatrix[(hitmatrix[:,0]==i)])
@@ -51,58 +90,47 @@ class HitDisplay:
             
         return (DC, Hodo, propTube)
 
-    def Raw_Hit(self, elementid, detectorid, selectedEvents, sid, eventID,ith_event):
-
-        eventIndex = np.where(selectedEvents[ith_event] == eventID)[0]
- 
+    def Raw_Hit(self):
+        '''creates a scatter plot item of the raw hits from the detector'''
+        eventIndex = np.where(self.selectedEvents[self.ith_event] == self.eventID)[0]
         # Create a scatter plot item
-        scatter = pg.ScatterPlotItem(detectorid[eventIndex][0],elementid[eventIndex][0], pen=pg.mkPen(None), symbol='o', size=10, brush=pg.mkBrush(128, 128, 128, 50))
+        scatter = pg.ScatterPlotItem(self.detectorid[eventIndex][0], self.elementid[eventIndex][0], pen=pg.mkPen(None), symbol='o', size=10, brush=pg.mkBrush(128, 128, 128, 50))
         return scatter
-    
-    def Cluster_Hit(self,hits, selectedEvents, sid, eventID,ith_event):
-        eventIndex = np.where(selectedEvents[ith_event] == eventID)[0]
-        cluster = np.vstack((np.where(hits[eventIndex]==True)[1],np.where(hits[eventIndex]==True)[2])).T
+
+    def Cluster_Hit(self):
+        '''creates a scatter plot item of the hits after declustering from Qtracker'''
+        eventIndex = np.where(self.selectedEvents[self.ith_event] == self.eventID)[0]
+        cluster = np.vstack((np.where(self.hits[eventIndex]==True)[1], np.where(self.hits[eventIndex]==True)[2])).T
         #shift detector from 0 to 1
         cluster[:,0] = cluster[:,0]+1
-        # detectorid = np.arange(0,55)
-        # print(elementID)
-        
-        # # Create a scatter plot item
-        scatter = pg.ScatterPlotItem(cluster[:,0],cluster[:,1], pen=pg.mkPen(None), symbol='o', size=10, brush=pg.mkBrush(255, 255, 255, 80))
+        # Create a scatter plot item
+        scatter = pg.ScatterPlotItem(cluster[:,0], cluster[:,1], pen=pg.mkPen(None), symbol='o', size=10, brush=pg.mkBrush(255, 255, 255, 80))
         return scatter
-    
-    def Track_Hits(self, selectedEvents, sid, eventID, track, ith_event):
 
-        elementid_mup = track[ith_event][:34,0]
-        elementid_mum = track[ith_event][34:,0]
+    def Track_Hits(self):
+        '''A scatter plot of the tracks selected by QTracker'''
+        elementid_mup = self.track[self.ith_event][:34,0]
+        elementid_mum = self.track[self.ith_event][34:,0]
         st1 = np.arange(1,7)
         st2 = np.arange(13,19)
         st3m = np.arange(19,25)
         st3p = np.arange(25,31)
-        hodo = np.arange(31,39) 
+        hodo = np.arange(31,39)
         prop = np.arange(47,55)
         if elementid_mup[13] > 0:
-            detectorid_mup = np.concatenate((st1,st2,st3p,hodo,prop))
+            detectorid_mup = np.concatenate((st1, st2, st3p, hodo, prop))
         else:
-            detectorid_mup = np.concatenate((st1,st2,st3m,hodo,prop))
+            detectorid_mup = np.concatenate((st1, st2, st3m, hodo, prop))
 
         if elementid_mum[13] > 0:
-            detectorid_mum = np.concatenate((st1,st2,st3p,hodo,prop))
+            detectorid_mum = np.concatenate((st1, st2, st3p, hodo, prop))
         else:
-            detectorid_mum = np.concatenate((st1,st2,st3m,hodo,prop))
+            detectorid_mum = np.concatenate((st1, st2, st3m, hodo, prop))
 
         elementid_mup = np.abs(elementid_mup)
         elementid_mum = np.abs(elementid_mum)
 
-        scatter_mup = pg.ScatterPlotItem(detectorid_mup,elementid_mup, pen=pg.mkPen(None), symbol='s', size=10, brush=pg.mkBrush(255, 0, 0, 120))
-        scatter_mum = pg.ScatterPlotItem(detectorid_mum,elementid_mum, pen=pg.mkPen(None), symbol='s', size=10, brush=pg.mkBrush(0, 255, 0, 120))
+        scatter_mup = pg.ScatterPlotItem(detectorid_mup, elementid_mup, pen=pg.mkPen(None), symbol='s', size=10, brush=pg.mkBrush(255, 0, 0, 120))
+        scatter_mum = pg.ScatterPlotItem(detectorid_mum, elementid_mum, pen=pg.mkPen(None), symbol='s', size=10, brush=pg.mkBrush(0, 255, 0, 120))
 
         return scatter_mup, scatter_mum
-
-
-
-
-
-
-
-
--- app/tabs/tab1.py
+++ app/tabs/tab1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import os
 
-# External Packages | PyQTt5
+# External Packages | PyQt5
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PyQt5.QtCore import Qt, QTimer
 import pyqtgraph as pg
@@ -11,13 +11,9 @@ import pyqtgraph as pg
 from app.modules.HitDisplay import HitDisplay
 from app.modules.DataOrganizer import DataOrganizer
 
-# Modules | MyTable
-# from app.modules.MyTable import MyTable
-
 class Tab1(QWidget):
 
     def __init__(self):
-
         super().__init__()
         self.initUI()
 
@@ -27,21 +23,21 @@ class Tab1(QWidget):
         layout.addWidget(self.plot_widget)  # Add the plot widget to the layout
 
         # Create QLabel for readout
-        self.readout_label = QLabel(alignment = Qt.AlignLeft)
+        self.readout_label = QLabel(alignment=Qt.AlignLeft)
         layout.addWidget(self.readout_label)
 
-        # # Enable antialiasing for prettier plots
-        pg.setConfigOptions(antialias = True)
+        # Enable antialiasing for prettier plots
+        pg.setConfigOptions(antialias=True)
 
-        #Raw Directory with root files from detector
+        # Raw Directory with root files from detector
         path_to_raw_directory = os.path.join("app/data/raw")
 
         # Initialize seen files set
         self.seen_files = set(os.listdir(path_to_raw_directory))
 
-        #Create an instance of dataOrganizer
+        # Create an instance of dataOrganizer
         self.organizer = DataOrganizer()
-        #call tab displays
+        # Call tab displays
         self.tab()
 
         # Setup a timer to check for new files repeatedly
@@ -49,11 +45,8 @@ class Tab1(QWidget):
         self.file_check_timer.timeout.connect(lambda: self.check_new_files(path_to_raw_directory))
         self.file_check_timer.start(40000)  # Check for new files every 40 seconds
 
-
-
-
-
     def check_new_files(self, directory):
+        '''Watches the raw directory for new files'''
         # Current set of files in the directory
         current_files = set(os.listdir(directory))
         
@@ -67,89 +60,14 @@ class Tab1(QWidget):
             self.seen_files.update(new_files)
 
             self.tab()
-            
-            
 
     def hit_display(self):
+        '''creates the hit_display for the main window.'''
         elementid, detectorid, selectedEvents, sid, hits, eventID, track = self.organizer.grab_HitInfo()
-        if self.ith_event >= len(selectedEvents):
-            self.timer.stop() 
-            return
-        
-        hitmatrices = HitDisplay()
-        scatter_raw = hitmatrices.Raw_Hit(elementid, detectorid, selectedEvents, sid, eventID, self.ith_event)
-        scatter_cluster = hitmatrices.Cluster_Hit(hits, selectedEvents, sid, eventID, self.ith_event)
-        scatter_mup, scatter_mum = hitmatrices.Track_Hits(selectedEvents, sid, eventID, track, self.ith_event)
-        
-        # Clear the plot widget before drawing new data
-        self.plot_widget.clear()
-        # Add the scatter plot items to the plot widget
-        self.plot_widget.addItem(scatter_raw)
-        self.plot_widget.addItem(scatter_cluster)                 
-        self.plot_widget.addItem(scatter_mup)
-        self.plot_widget.addItem(scatter_mum)
-
-        # Set the y-axis limit to 0, 201
-        self.plot_widget.setYRange(0, 201)
-        #print("Hit Display plotted")
-
-        # Advance the event index
-        self.ith_event += 1
-
-
-    # def draw_hitmatrix(self):
-
-    #     # ith_event = 0
-    #      elementid, detectorid, selectedEvents, sid, hits, eventID, track= self.organizer.grab_HitInfo()
-    #      hitmatrices = HitDisplay()
-    #     # scatter_raw = hitmatrices.Raw_Hit(elementid,detectorid, selectedEvents, sid, eventID, ith_event)
-    #     # scatter_cluster = hitmatrices.Cluster_Hit(hits,selectedEvents,sid,eventID, ith_event)
-    #     # scatter_mup, scatter_mum = hitmatrices.Track_Hits(selectedEvents,sid,eventID,track, ith_event)
-    #     # #  #Create an instance of dataOrganizer
-    #     # organizer = DataOrganizer()
-    #     # # Clear the plot widget before drawing new data
-    #     # self.plot_widget.clear()
-    #     # # Add the scatter plot items to the plot widget
-    #     # self.plot_widget.addItem(scatter_raw)
-    #     # self.plot_widget.addItem(scatter_cluster)
-    #     # self.plot_widget.addItem(scatter_mup)
-    #     # self.plot_widget.addItem(scatter_mum)
-
-    #     #Get Occupancy 
-    #     DC, Hodo, propTube = hitmatrices.getOcc(hits,selectedEvents, eventID,ith_event)
-
-    #     #get precentage per spill
-    #     nelem_DC = np.array([201,201,160,160,201,201,384,384,320,320,384,384,128,128,112,112
-    #                       ,128,	128,134,134,116,116,134,134,134,134,116,116,134,134])
-  
-    #     nelem_hodo = np.array([20,20,23,23,	19,	19,	16,	16,	16,16,16,	16,16,	16,	16,	16,])
-    #     nelem_prop = 8
-
-    #     DC_Percentage = (DC/nelem_DC)*100
-    #     Avg_DC_Occ = np.average(DC_Percentage)
-    #     Total_DC = np.sum(DC)
-    #     hodo_Percentage = (Hodo/nelem_hodo)*100
-    #     Avg_hodo_Occ = np.average(hodo_Percentage)
-    #     Total_hodo = np.sum(Hodo)
-    #     prop_Percentage = (propTube/nelem_prop)*100
-    #     Avg_prop_Occ = np.average(prop_Percentage)
-    #     Total_prop = np.sum(propTube)
-
-    #     total_avg_per_spill = Avg_DC_Occ+Avg_hodo_Occ+Avg_prop_Occ
-    #     total_occ_per_spill = Total_DC+Total_hodo+Total_prop
-
-    #     text = f"Occupancy\nAvg Hits per Event: {total_avg_per_spill:.0f}%\nTotal Hits per Spill: {total_occ_per_spill}\n"
-    #     self.readout_label.setText(text)
+        self.hit_display_instance = HitDisplay(elementid, detectorid, selectedEvents, sid, hits, eventID, track, plot_widget=self.plot_widget)
 
     def tab(self):
+        '''Calls all displays for the tab.'''
         # Reorganize data and update displays
-            self.organizer.organizeData()
-
-            #Loop through events in Hit Display
-            self.ith_event = 0
-            self.timer = QTimer(self)
-            self.timer.timeout.connect(self.hit_display)
-            self.timer.start(1000)  # Call hit_display every 1000 milliseconds (1 second)
-
-        
-       # self.readout_table()
+        self.organizer.organizeData()
+        self.hit_display()
